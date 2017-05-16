245b283c4d0bcfe9ed13048c1dcc4d76c554eb76
大村 豪 <omura@omura-NB733.local>
2017-05-16 16:08:04 +0900

    フォローワーごとにタイムライン取得完了

--- handler/handler.go
+++ handler/handler.go
@@ -5,10 +5,11 @@ import (
 	"instagram/db"
 	"instagram/model"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
-	//"github.com/gocraft/dbr"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gocraft/dbr"
 )
 
 var (
@@ -23,20 +24,82 @@ var (
 
 func SelectUsers(c echo.Context) error {
 
-        if err != nil {
-            panic(fmt.Errorf("DB connection error: %s \n", err))
-        }
+	if err != nil {
+		panic(fmt.Errorf("DB connection error: %s \n", err))
+	}
 
-	var u []model.User
+	var u []model.UserResponse
 	_, err = sess.Select("*").From("user").Load(&u)
 	if err != nil {
 		fmt.Println(err.Error())
 		return c.JSON(http.StatusOK, err.Error())
 	} else {
-		response := new(model.ResponseData)
-		response.Users = u
-		return c.JSON(http.StatusOK, response)
+		return c.JSON(http.StatusOK, u)
 	}
+}
+
+func GetTimeline(c echo.Context) error {
+
+	if err != nil {
+		panic(fmt.Errorf("DB connection error: %s \n", err))
+	}
+	var id int64
+	param := c.Param("id")
+	date := c.Param("date")
+	id, err = strconv.ParseInt(param, 0, 64)
+
+	//var u []model.UserResponse
+	//_, err = sess.Select("*").From("user").Where("user_id = ?",id).Load(&u)
+
+	var timeline []model.TimelineResponse
+	count, err := sess.Select("m.*").From(dbr.I("follow_list").As("f")).
+		Join(dbr.I("media").As("m"), "f.user_id = m.user_id").
+		Where("f.my_id = ?", id).
+		OrderDir("m.created_time", false).
+		Limit(10).Load(&timeline)
+
+	if count == 0 {
+		return c.JSON(http.StatusOK, "表示するタイムラインがありません")
+	}
+
+	for key, value := range timeline {
+		var user []model.UserResponse
+		var likes []model.LikesResponse
+		var likeCount = 0
+		var isLiked = 0
+
+		_, err = sess.Select("u.*").From(dbr.I("media").As("m")).
+			Join(dbr.I("user").As("u"), "u.user_id = m.user_id").Where("u.user_id = ?", value.UserID).Load(&user)
+
+		value.User = user
+
+		likeCount, err = sess.Select("*").From(dbr.I("media").As("m")).
+			Join(dbr.I("like").As("l"), "l.media_id = m.media_id").Where("l.media_id = ?", value.MediaID).Load(&likes)
+
+		isLiked, err = sess.Select("*").From(dbr.I("media").As("m")).
+			Join(dbr.I("like").As("l"), "l.media_id = m.media_id").Where("l.media_id = ? AND l.user_id = ?", value.MediaID, id).Load(&likes)
+
+
+		value.LikeCounts = likeCount
+
+		if isLiked > 0 {
+			value.IsLiked = true
+		}
+
+		fmt.Println("count", isLiked)
+		fmt.Println("count", isLiked)
+		fmt.Println("like", value)
+		timeline[key] = value
+
+	}
+	//"u.full_name","u.username","u.profile_picture" Where("u.user_id = ?", id)
+	if err != nil {
+		fmt.Println(err.Error())
+		return c.JSON(http.StatusOK, err.Error())
+	} else {
+		return c.JSON(http.StatusOK, timeline)
+	}
+
 }
 
 func InsertUser(c echo.Context) error {
--- main.go
+++ main.go
@@ -21,8 +21,11 @@ func main() {
 
 	// Routes
 	e.GET("/users", handler.SelectUsers)
+	e.GET("/timeline/:id/:date", handler.GetTimeline)
+
 	//e.POST("/users", InsertUser)
 
+	//fmt.Println(handler.GetTimeline)
 	// Start server
 	viper.SetDefault("http.port", 1323)
 	port := fmt.Sprintf(":%d", viper.GetInt("http.port"))
